deaf4509c6952d96e77734df3d210aca7f415221
zarghamkhandev <zarghamkhan980@gmail.com>
2020-07-22 13:46:09 +0200

    initialization

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import Recipe from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test recipe',
+      'this is simple a test',
+      'https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1295&q=80'
+    ),
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
